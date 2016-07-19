98645138514d79e9ae5c10f13a2409f5fa13d7a8
Gio d'Amelio <giodamelio@gmail.com>
2016-07-18 22:42:46 -0700

    Seperate message formatting into seperate function. Add connect and disconnect messages

--- lab-giodamelio/lib/clientPool.js
+++ lab-giodamelio/lib/clientPool.js
@@ -17,7 +17,7 @@ function ClientPool() {
   // Add new client
   this.on('connect', (client) => {
     const id = shortid.generate();
-    console.log(`Connection (id: ${id})`);
+    console.log(`Connect: (id: ${id})`);
     client.id = id;
     client.nick = `user_${id}`;
     this.clients[id] = client;
@@ -25,29 +25,27 @@ function ClientPool() {
     // Pick out a color for the user
     client.color = colors[Math.floor(colors.length * Math.random())];
 
+    // Send welcome message
     client.write(`Welcome to the ${chalk.underline.bold.white('BEST')} chat server ever!
 For a list of available commands run /help\n`);
+
+    // Announce user
+    this.emit('broadcast', 'server', `${client.nick} has joined\n`);
   });
 
   // Remove a client
   this.on('disconnect', (client) => {
-    console.log(`Disconnect (id: ${client.id})`);
+    console.log(`Disconnect: (id: ${client.id})`);
     delete this.clients[client.id];
+    this.emit('broadcast', 'server', `${client.nick} has quit\n`);
   });
 
   // Broadcast a message to all clients
   this.on('broadcast', (sender, message) => {
+    process.stdout.write(`Message: ${this.formatMessage(sender, message.toString())}`);
     for (const id of Object.keys(this.clients)) {
       const client = this.clients[id];
-      if (sender === 'server') {
-        const serverName = chalk.white.underline.bold('SERVER');
-        client.write(`${serverName}: `);
-        client.write(message);
-      } else if (sender.id !== client.id) {
-        const username = chalk[sender.color].underline(sender.nick);
-        client.write(`${username}: `);
-        client.write(message);
-      }
+      client.write(this.formatMessage(sender, message));
     }
   });
 
@@ -69,13 +67,23 @@ For a list of available commands run /help\n`);
         return;
       }
 
-      console.log(`Changing nick from ${sender.nick} to ${args[0]}`);
+      console.log(`Command: changing nick from ${sender.nick} to ${args[0]}`);
       this.emit('broadcast', 'server', `${sender.nick} changed their nickname to ${args[0]}\n`);
       sender.nick = args[0];
       return;
     }
   });
 }
+
+ClientPool.prototype.formatMessage = function (sender, message) {
+  if (sender === 'server') {
+    const serverName = chalk.white.underline.bold('SERVER');
+    return `${serverName}: ${message}`;
+  }
+
+  const username = chalk[sender.color].underline(sender.nick);
+  return `${username}: ${message}`;
+};
 
 util.inherits(ClientPool, EE);
 
